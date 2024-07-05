cc7bda0d5e8f184a65d988162f1c4d6817c9f8df
chelizichen <1347290221@qq.com>
2024-07-06 00:33:04 +0800

    (update) sgrid vite plugin

--- static/app.ts
+++ static/app.ts
@@ -1,8 +1,9 @@
 import { NewSgridServer, NewSgridServerCtx } from "sgridnode/build/main";
 import express from 'express'
+import path from 'path';
 function boost() {
   const ctx = NewSgridServerCtx();
-  ctx.use('/web',express.static("./public"))
+  ctx.use('/docs/',express.static(path.resolve(__dirname,"./dist") ))
   NewSgridServer(ctx);
 }
 
